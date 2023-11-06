6c5f31507a3840ac2bf705f1dfa1a9cd93e20f28
gaddargang <149903492+gaddargang@users.noreply.github.com>
2023-11-07 01:06:33 +0530

    Update fibo.py

--- DAA/1 Fibonacci Series/fibo.py
+++ DAA/1 Fibonacci Series/fibo.py
@@ -1,13 +1,13 @@
 import timeit
 
 def fibonacci(n):
-    """Non-recursive fibonacci function"""
+    """Non-recursive Fibonacci function"""
     for i in range(2, n + 1):
         fib_list[i] = fib_list[i - 1] + fib_list[i - 2]
     return fib_list[n]
 
 def fibonacci_recursive(n):
-    """Recursive fibonacci function"""
+    """Recursive Fibonacci function"""
     if n == 0:
         return 0
     if n == 1:
@@ -15,21 +15,21 @@ def fibonacci_recursive(n):
     fib_recur_list[n] = fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
     return fib_recur_list[n]
 
-N = int(input("Enter the number of terms in series : "))
+N = int(input("Enter the number of terms in the series: "))
 
 RUNS = 1
 fib_list = [0] * (N + 1)
 fib_list[0] = 0
 fib_list[1] = 1
 print("Fibonacci non-recursive:")
-for i in range(1, N + 1):  # Corrected loop range
-    print(f"F({i}) = {fibonacci(i)}")
-print("Time:", f'{timeit.timeit("fibonacci(N)", setup=f"from __main__ import fibonacci;N={N}", number=RUNS):5f}', "O(n)\tSpace: O(1)")
+for i in range(N):
+    print(fibonacci(i), end=' ')  # Print numbers only
+print("\nTime:", f'{timeit.timeit("fibonacci(N)", setup=f"from __main__ import fibonacci;N={N}", number=RUNS):5f}', "O(n)\tSpace: O(1)")
 
 fib_recur_list = [0] * (N + 1)
 fib_recur_list[0] = 0
 fib_recur_list[1] = 1
 print("\nFibonacci recursive:")
-for i in range(1, N + 1):  # Corrected loop range
-    print(f"F({i}) = {fibonacci_recursive(i)}")
-print("Time:", f'{timeit.timeit("fibonacci_recursive(N)", setup=f"from __main__ import fibonacci_recursive;N={N}", number=RUNS):5f}', "O(2^n)\tSpace: O(n)")
+for i in range(N):
+    print(fibonacci_recursive(i), end=' ')  # Print numbers only
+print("\nTime:", f'{timeit.timeit("fibonacci_recursive(N)", setup=f"from __main__ import fibonacci_recursive;N={N}", number=RUNS):5f}', "O(2^n)\tSpace: O(n)")
